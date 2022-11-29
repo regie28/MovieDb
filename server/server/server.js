f13886c52fd87e79448e658127095fb4c561af17
regie28 <ghostrider072017@gmail.com>
2022-11-29 14:23:41 +0800

    backend dependencies

---
+++ server/server/server.js
@@ -0,0 +1,71 @@
+//import module/libraries
+const express = require('express')
+const parser = require('body-parser')
+const { application } = require('express')
+const Pool = require('pg').Pool
+
+//initialize backend
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+const pool = new Pool(
+    {
+        user : 'postgres',
+        host: 'localhost',
+        database : 'movieDB',
+        password : '1234',
+        port : 5433
+    }
+)
+
+app.get('/',(req, res) => {
+    res.json(
+        {
+            info : 'Backend Running'
+        }
+    )
+})
+
+//Get all category
+
+app.get('/movie', async (req, res) => {
+   try {
+    const movie = await pool.query("SELECT * FROM public.movie ORDER BY movieid ASC")
+    res.json(movie.rows)
+   } catch (error) {
+    console.log(error)
+   }
+})
+
+app.get('/tvshow', async (req, res) => {
+    try {
+     const tvshow = await pool.query("SELECT * FROM public.tvshow ORDER BY tvshowid ASC")
+     res.json(tvshow.rows)
+    } catch (error) {
+     console.log(error)
+    }
+ })
+//Get all Product
+app.get('/product', async (req, res) => {
+    try {  
+        const product = await pool.query("SELECT * FROM public.product ORDER BY productid ASC")
+        res.json(product.rows)
+    } catch (error) {
+        console.log(error)
+    }
+    })
+//Get all User
+app.get('/usermdb', async (req, res) => {
+    try {
+        const usermdb = await pool.query("SELECT * FROM public.usermdb ORDER BY userid ASC")
+        res.json(usermdb.rows)
+    } catch (error) {
+        console.log(error)
+    }
+    })
+    
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`)
+});
